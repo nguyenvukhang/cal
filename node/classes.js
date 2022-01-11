67319319274c4dfa8d5625d8a932fc174ea0eb25
nguyenvukhang <brew4k@gmail.com>
2022-01-11 12:30:19 +0800

    made a semester-long module generator

--- node/classes.js
+++ node/classes.js
@@ -1,8 +1,55 @@
+import moment from 'moment'
+import ics from 'ics'
+import modules from './modules.js'
 
-const MA2219 = {
-  start: [2022, 1, 10, 16],
-  duration: { hours: 2 },
-  title: 'MA2219: LEC',
-  location: 'LT28',
-  status: 'CONFIRMED',
+const semStart = moment('2022-01-10', 'YYYY-MM-DD')
+
+function dayNum(str) {
+  const day = { mon: 0, tue: 1, wed: 2, thu: 3, fri: 4, sat: 5, sun: 6 }
+  const s = str.slice(0, 3).toLowerCase()
+  return day[s]
 }
+
+function makeEvents(modules, semStart) {
+  const res = []
+  modules.forEach((module) => {
+    module.weeks.forEach((w, i) => {
+      if (w) {
+        const date = semStart.clone()
+        date.add(i, 'weeks')
+        const events = module.timeslots.map((ts) => {
+          const [year, month, day] = date
+            .add(dayNum(ts.day), 'days')
+            .format('YYYY-MM-DD')
+            .split('-')
+            .map((e) => parseInt(e))
+          const [hour, minute] = ts.start.split(':').map((e) => parseInt(e))
+          return {
+            start: [year, month, day, hour, minute],
+            title: ts.type ? `${module.name}: ${ts.type}` : ts.title,
+            location: ts.location,
+            duration: ts.duration,
+          }
+        })
+        res.push(...events)
+      }
+    })
+  })
+
+  return res
+}
+
+const res = makeEvents(modules, semStart)
+
+function writeICS(res) {
+  const { error, value } = ics.createEvents(res)
+
+  if (error) {
+    console.log(error)
+  } else {
+    console.log(value)
+  }
+}
+
+const whackICS = true
+whackICS ? writeICS(res) : console.log(res)
