d6c37b2a20759721dbafbc621a1ed1dd057d075c
nguyenvukhang <brew4k@gmail.com>
2022-07-12 11:55:57 +0800

    refactor

--- test/timetable.test.ts
+++ test/timetable.test.ts
@@ -1,33 +1,42 @@
 import { timetable } from '../src/get-timetable'
+import { LessonData, Timeslot } from '../src/types'
 import { validModuleCode } from '../src/utils'
+
+function assertTimeslots(timeslots: Timeslot[]) {
+  describe('timeslots', () => {
+    expect(timeslots).toBeInstanceOf(Array)
+    timeslots.forEach((ts, tsIndex) => {
+      test(`data #${tsIndex}`, () => {
+        expect([
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday',
+        ]).toContain(ts.day)
+      })
+    })
+    // weeks.forEach((n) => expect(typeof n).toBe('string'))
+  })
+}
+
+function assertWeeks(index: number, lessonData: LessonData) {
+  const { weeks, timeslots } = lessonData
+  describe(`week data #${index}`, () => {
+    /** weeks should be an array of numbers */
+    expect(weeks).toBeInstanceOf(Array)
+    weeks.forEach((n) => expect(typeof n).toBe('number'))
+    assertTimeslots(timeslots)
+  })
+}
 
 describe.each(timetable.moduleCodes)('%s', (code) => {
   expect([code, validModuleCode(code)]).toEqual([code, true])
   const lessonData = timetable.getLessons(code)
-  lessonData.forEach(({ weeks, timeslots }, index) => {
-    /** weeks should be an array of numbers */
-    test(`week data #${index}`, () => {
-      expect(weeks).toBeInstanceOf(Array)
-      weeks.forEach((n) => expect(typeof n).toBe('number'))
-    })
-    describe('timeslots', () => {
-      if (code === 'HSS1000') console.log(timeslots)
-      expect(timeslots).toBeInstanceOf(Array)
-      timeslots.forEach((ts, tsIndex) => {
-        test(`data #${tsIndex}`, () => {
-          expect([
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday',
-            'Sunday',
-          ]).toContain(ts.day)
-        })
-      })
-      // weeks.forEach((n) => expect(typeof n).toBe('string'))
-    })
+  lessonData.forEach((lessonData, index) => {
+    assertWeeks(index, lessonData)
   })
 })
 
