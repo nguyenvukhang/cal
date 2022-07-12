695d77d6719eae85997691fadb858e1e5ea72c0d
nguyenvukhang <brew4k@gmail.com>
2022-07-12 11:02:09 +0800

    create ics for weeks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import moment from 'moment'
+import { EventAttributes, createEvents } from 'ics'
 import type { Moment } from 'moment'
 import weekNames from '../data/json/weeks.json'
+import fs from 'fs'
+
+type DateWithWeekName = [string, string]
+type AcademicYearByWeek = DateWithWeekName[]
+
+const log = console.log
 
 const weekZeroMonday = moment('01-08-2022', 'DD-MM-YYYY')
 
@@ -18,12 +25,39 @@ const weekZeroMonday = moment('01-08-2022', 'DD-MM-YYYY')
 function getDatesWithWeekNames(
   weekNames: string[],
   weekZeroMonday: Moment
-): [string, string][] {
+): AcademicYearByWeek {
   return weekNames.map((name, index) => {
     const date = weekZeroMonday.clone().add(index, 'weeks').format('YYYY-MM-DD')
     return [date, name]
   })
 }
 
-const dates = getDatesWithWeekNames(weekNames, weekZeroMonday)
-console.log(dates)
+/**
+ * useful to convert YYYY-MM-DD to [YYYY, MM, DD]
+ */
+function dashedStringToNumberedArray(str: string): number[] {
+  return str.split('-').map((e) => parseInt(e))
+}
+
+function getIcsEvents(
+  academicYearByWeek: AcademicYearByWeek
+): EventAttributes[] {
+  return academicYearByWeek.map(([date, name]) => {
+    const [y, m, d] = dashedStringToNumberedArray(date)
+    return {
+      start: [y, m, d],
+      title: name,
+      status: 'CONFIRMED',
+      duration: {},
+    }
+  })
+}
+
+function main() {
+  const academicYearByWeek = getDatesWithWeekNames(weekNames, weekZeroMonday)
+  const icsEvents = getIcsEvents(academicYearByWeek)
+  const icsDump: string = createEvents(icsEvents).value || ''
+  fs.writeFileSync('debug2.ics', icsDump)
+}
+
+main()
